54c7e4977a64c088c5843f096c422136ddac027e
rebecca martinho <rebeccamartinho@hotmail.com>
2021-03-23 16:34:22 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+import Converter from './components/Converter'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container"></div>
+      <h1>Conversor de moedas</h1>
+      <div className="line">
+        <Converter currencyA="USD" currencyB="BRL" />
+        <Converter currencyA="BRL" currencyB="USD" />
+        </div>
+      <div className="line">
+        <Converter currencyA="CAD" currencyB="BRL" />
+        <Converter currencyA="BRL" currencyB="CAD" />
+      </div>
+      <div className="line">
+        <Converter currencyA="EUR" currencyB="BRL" />
+        <Converter currencyA="BRL" currencyB="EUR" />
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Converter.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import './Converter.css'
+
+export default class Converter extends Component {
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currencyA_value: "",
+      currencyB_value: 0,
+    }
+    this.convert = this.convert.bind(this);
+    
+  }
+
+  convert() {
+    let from_to = `${this.props.currencyA}_${this.props.currencyB}`;
+    let url = `https://free.currconv.com/api/v7/convert?q=${from_to}&compact=ultra&apiKey=f8543ecca1d339d875fa`;
+
+  fetch(url)
+  .then(res=>{
+    return res.json()
+  })
+  .then(json=>{
+    
+    let quotation = json[from_to];
+    let currencyB_value =( parseFloat(this.state.currencyA_value) * quotation).toFixed(2);
+    this.setState({currencyB_value})
+
+  })
+}
+
+  render() {
+    return (
+      <div className="converter">
+        <h2>{this.props.currencyA} para {this.props.currencyB}</h2>
+        <input 
+          type="text" 
+          onChange={(event) => {this.setState({currencyA_value:event.target.value})}}>
+        </input>
+        <button type="button" value="Convert" onClick = {this.convert}>Converter</button>
+        <h2>Valor convertido: {this.state.currencyB_value}</h2>
+      
+      </div>
+      
+    );
+  }
+}
