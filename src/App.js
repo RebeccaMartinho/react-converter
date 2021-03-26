6685480333be127e6b1194bf719a6efcd51ab77e
rebecca martinho <rebeccamartinho@hotmail.com>
2021-03-26 11:32:31 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,27 +1,82 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import Converter from './components/Converter'
 
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
+
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+  
+
+  let toAmount, fromAmount
+    if(amountInFromCurrency) {
+      fromAmount = amount
+      toAmount = amount * exchangeRate
+    } else {
+      toAmount = amount
+      fromAmount = amount / exchangeRate
+    }
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
+      })
+  }, [])
+
+  useEffect(() => {
+    if(fromCurrency != null & toCurrency !=null){
+      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then(res => res.json())
+        .then(data => setExchangeRate(data.rates[toCurrency]))
+    } 
+  }, [fromCurrency, toCurrency])
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
   return (
     <div className="App">
-      
-      <h1>Conversor de moedas</h1>
+      <h1 className="title">Conversor de moedas</h1>
       <div className="line">
-        <Converter currencyA="USD" currencyB="BRL" />
-        <Converter currencyA="BRL" currencyB="USD" />
+        <Converter 
+          currencyOptions={currencyOptions}
+          selectedCurrency={fromCurrency} 
+          onChangeCurrency={e => setFromCurrency(e.target.value)}
+          onChangeAmount={handleFromAmountChange}
+          amount={fromAmount}
+        />  
+        <div className="equals">=</div>
+        <Converter 
+          currencyOptions={currencyOptions}  
+          selectedCurrency={toCurrency}
+          onChangeCurrency={e => setToCurrency(e.target.value)}
+          onChangeAmount={handleToAmountChange}
+          amount={toAmount}
+          />
         </div>
-      <div className="line">
-        <Converter currencyA="CAD" currencyB="BRL" />
-        <Converter currencyA="BRL" currencyB="CAD" />
-      </div>
-      <div className="line">
-        <Converter currencyA="EUR" currencyB="BRL" />
-        <Converter currencyA="BRL" currencyB="EUR" />
-      </div>
-    </div>
-    
+    </div>   
   );
 }
 
 export default App;
+
+
--- src/components/Converter.js
+++ src/components/Converter.js
@@ -1,51 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Converter.css'
 
-export default class Converter extends Component {
-  
-  constructor(props){
-    super(props);
+export default function Converter(props){
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount
+  } = props
 
-    this.state = {
-      currencyA_value: "",
-      currencyB_value: 0,
-    }
-    this.convert = this.convert.bind(this);
-    
-  }
-
-  convert() {
-    let from_to = `${this.props.currencyA}_${this.props.currencyB}`;
-    let url = `https://free.currconv.com/api/v7/convert?q=${from_to}&compact=ultra&apiKey=f8543ecca1d339d875fa`;
-
-  fetch(url)
-  .then(res=>{
-    return res.json()
-  })
-  .then(json=>{
-    
-    let quotation = json[from_to];
-    let currencyB_value =( parseFloat(this.state.currencyA_value) * quotation).toFixed(2);
-    this.setState({currencyB_value})
-
-  })
-}
-
-  render() {
-    return (
+  return (
       <div className="converter">
-        <h2>{this.props.currencyA} para {this.props.currencyB}</h2>
         <input 
-          type="text"
-          placeholder="Valor a ser convertido" 
-          onChange={(event) => {this.setState({currencyA_value:event.target.value})}}>
-        </input><br/>
-        <button type="button" value="Convert" onClick = {this.convert}>Converter</button>
-        
-        <h2>Valor convertido: {this.state.currencyB_value}</h2>
+           className="input"         
+           type="number"           
+           step="0.01"            
+           value={amount}
+           onChange={onChangeAmount}
+           />
       
-      </div>
-      
-    );
-  }
+         <select className="select" value={selectedCurrency} onChange={onChangeCurrency}>
+           {currencyOptions.map(option => (
+            <option key={option} value={option}>{option}</option>
+           ))}         
+         </select>
+        </div>
+  )
 }
+
+
